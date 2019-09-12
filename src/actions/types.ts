5612a7d84c240c0c58c0ddb0dc957faeb95ba915
tanshio <rakuburomail+tanshio@gmail.com>
2019-09-12 20:01:05 +0900

    WIP redux

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,4 +1,5 @@
 import { AnyAction } from 'redux'
+import * as actions from './actions'
 
 export type ActionsType<ActionCreators extends object> = {
   [Key in keyof ActionCreators]: ActionCreators[Key] extends (
@@ -12,3 +13,5 @@ export type ActionType<
   ActionCreators extends object,
   Actions = ActionsType<ActionCreators>
   > = { [Key in keyof Actions]: Actions[Key] }[keyof Actions]
+
+export type Action = ActionType<typeof actions>
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,3 +1,23 @@
 import { combineReducers } from "redux"
+import { Action } from "../actions/types"
 
-export const rootReducer = combineReducers({})
+type State = {
+  count: number
+}
+
+const initialValue = {
+  count: 0,
+}
+
+export const reducer = (state: State = initialValue, action: Action): State => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + action.payload }
+    case "decrement":
+      return { count: state.count - action.payload }
+    default:
+      return state
+  }
+}
+
+export const rootReducer = combineReducers(reducer)
